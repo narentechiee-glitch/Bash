d17974c86919d94ea5602663546b876a4b30f2c5
Naren <narentechiee@gmail.com>
2026-05-17 21:45:18 +0530

    welcome dolly

---
+++ naren.js
@@ -0,0 +1,1 @@
+console.log("Hello Welcom to our Console");
---
+++ naren.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function naren() {
+  return (
+    <div>naren
+        <h1>Welcome React</h1>
+    </div>
+  )
+}
+
+export default naren
