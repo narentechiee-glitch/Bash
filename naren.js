207c41264036aec0536e8e99fbf1697a2317cedd
Naren <narentechiee@gmail.com>
2026-05-18 13:23:50 +0530

    New Chnages

--- naren.js
+++ naren.js
@@ -1,1 +1,6 @@
 console.log("Hello Welcom to our Console");
+ const Name=()=>{
+          console.log("Welcome");
+          
+        }
+        Name()
--- naren.jsx
+++ naren.jsx
@@ -4,6 +4,7 @@ function naren() {
   return (
     <div>naren
         <h1>Welcome React</h1>
+       
     </div>
   )
 }
