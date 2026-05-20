73140716aa74c272f15eb28521e952acb4638cc2
Naren <narentechiee@gmail.com>
2026-05-20 21:42:46 +0530

    GOT

--- naren.jsx
+++ naren.jsx
@@ -4,7 +4,9 @@ function naren() {
   return (
     <div>naren
         <h1>Welcome React</h1>
-       
+        <h1>I am Steve Rogers captain america of avengers</h1>
+        <h3>Jon Snow king in the north Winterfell i like House Of the Drogun </h3>
+        <button>Game of Thrones</button>
     </div>
   )
 }
